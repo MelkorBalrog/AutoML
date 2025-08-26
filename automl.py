f11e7f73155178fb2dca95d9563184070b4f6037
Capek System Safety & Robotic Solutions <96583804+MelkorBalrog@users.noreply.github.com>
2025-08-26 14:27:10 -0400

    Refactor structure tree operations into service

--- automl.py
+++ automl.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Project version information."""
+"""Lowercase wrapper re-exporting :mod:`AutoML` launcher."""
 
-VERSION = "0.2.118"
-
-__all__ = ["VERSION"]
+from AutoML import *  # noqa: F401,F403
